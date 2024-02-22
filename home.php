eb07bffaa3f2a60126f0fbf5416673a54fc50181
AMB <ambmedrano8@gmail.com>
2024-02-22 14:26:09 -0500

    Add plp design

--- home.php
+++ home.php
@@ -234,7 +234,7 @@
             </div>
         </div>
         <div class="good-time-section__button-container">
-            <button class="good-time-section__button-container--shop-all">SHOP ALL</button>
+            <a href="#" class=" btn btn__white--small">Shop all</a>
         </div>
     </div>
 </section>
@@ -308,22 +308,22 @@
         <div class="what-everyone-section__video-list slider3">
             <div class="what-everyone__card">
                 <video autoplay muted width="286" height="545">
-                    <source src="<?php echo get_template_directory_uri(); ?>/img/ssstik.io_1708112788862.mp4' }}" type="video/mp4">
+                    <source src="<?php echo get_template_directory_uri(); ?>/img/tiktok.mp4' }}" type="video/mp4">
                 </video>    
             </div>
             <div class="what-everyone__card">
                 <video autoplay muted width="286" height="545">
-                    <source src="<?php echo get_template_directory_uri(); ?>/img/ssstik.io_1708112788862.mp4' }}" type="video/mp4">
+                    <source src="<?php echo get_template_directory_uri(); ?>/img/tiktok.mp4' }}" type="video/mp4">
                 </video>    
             </div>
             <div class="what-everyone__card">
                 <video autoplay muted width="286" height="545">
-                    <source src="<?php echo get_template_directory_uri(); ?>/img/ssstik.io_1708112788862.mp4' }}" type="video/mp4">
+                    <source src="<?php echo get_template_directory_uri(); ?>/img/tiktok.mp4' }}" type="video/mp4">
                 </video>    
             </div>
             <div class="what-everyone__card">
                 <video autoplay muted width="286" height="545">
-                    <source src="<?php echo get_template_directory_uri(); ?>/img/ssstik.io_1708112788862.mp4' }}" type="video/mp4">
+                    <source src="<?php echo get_template_directory_uri(); ?>/img/tiktok.mp4' }}" type="video/mp4">
                 </video>    
             </div>
             <div class="what-everyone__card">
---
+++ page-plp.php
@@ -0,0 +1,234 @@
+<?php get_header(); ?>
+<div class="plp-hero" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/nowadays-hero-img.png')">
+    <div class="container">
+        <h2>A good time
+            <br>in no time
+        </h2>
+    </div>    
+</div>
+<div class="animated-text">
+    <div class="animated-text__container">
+        <p>
+            <span>DON’T DRINK HARD.</span>
+            DRINK EASY.
+        </p>
+        <p>
+            <span>DON’T DRINK HARD.</span>
+            DRINK EASY.
+        </p>
+        <p>
+            <span>DON’T DRINK HARD.</span>
+            DRINK EASY.
+        </p>
+        <p>
+            <span>DON’T DRINK HARD.</span>
+            DRINK EASY.
+        </p>
+        <p>
+            <span>DON’T DRINK HARD.</span>
+            DRINK EASY.
+        </p>
+        <p>
+            <span>DON’T DRINK HARD.</span>
+            DRINK EASY.
+        </p>
+        <p>
+            <span>DON’T DRINK HARD.</span>
+            DRINK EASY.
+        </p>
+        </div>
+</div>
+
+
+<section class="shop-nowadays">
+    <div class="container">
+        <div class="shop-nowadays__head">
+            <h2 class="shop-nowadays__title">SHOP NOWADAYS</h2>
+        </div>
+        <div class="shop-nowadays__products">
+            <div class="sn-product">
+                <div class="sn-product__image sn-product__one">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/Nowadays_micro_dose.webp'">
+                    <?php $background_product = get_field( 'background_product' ); ?>
+                </div>
+                <div class="sn-product__info">
+                    <h2 class="sn-product__title">Nowadays micro dose</h2>
+                    <div class="sn-product__reviews">
+                        <input type="radio" id="star5" name="rating" value="5"/>
+                        <label for="star5">&#9733;</label>
+                        <input type="radio" id="star4" name="rating" value="4"/>
+                        <label for="star4">&#9733;</label>
+                        <input type="radio" id="star3" name="rating" value="3"/>
+                        <label for="star3">&#9733;</label>
+                        <input type="radio" id="star2" name="rating" value="2"/>
+                        <label for="star2">&#9733;</label>
+                        <input type="radio" id="star1" name="rating" value="1"/>
+                        <label for="star1">&#9733;</label>
+                    </div>
+                    <span class="sn-product__span">67 reviews</span>
+                    <p class="sn-product__description">2mg thc drink</p>
+                    <p class="sn-product__bottle">750 ml/33mg per bottle</p>
+                    <p class="sn-product__price">$39.99</p>
+                    <a href="#" class=" btn btn__beige--small">Add to card</a>
+                </div>
+            </div>
+            <div class="sn-product">
+                <div class="sn-product__image sn-product__two">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/Nowadays_micro_dose.webp'">
+                </div>
+                <div class="sn-product__info">
+                    <h2 class="sn-product__title ">Nowadays low dose</h2>
+                    <div class="sn-product__reviews">
+                        <input type="radio" id="star10" name="rating1" value="5"/>
+                        <label for="star10">&#9733;</label>
+                        <input type="radio" id="star9" name="rating1" value="4"/>
+                        <label for="star9">&#9733;</label>
+                        <input type="radio" id="star8" name="rating1" value="3"/>
+                        <label for="star8">&#9733;</label>
+                        <input type="radio" id="star7" name="rating1" value="2"/>
+                        <label for="star7">&#9733;</label>
+                        <input type="radio" id="star6" name="rating1" value="1"/>
+                        <label for="star6">&#9733;</label>
+                    </div>
+                    <span class="sn-product__span">87 reviews</span>
+                    <p class="sn-product__description">6mg thc drink</p>
+                    <p class="sn-product__bottle">750 ml/100mg per bottle</p>
+                    <p class="sn-product__price">$59.99</p>
+                    <a href="#" class=" btn btn__beige--small">Add to card</a>
+                </div>
+            </div>
+            <div class="sn-product">
+                <div class="sn-product__image sn-product__three">
+                    <img class="bs-product__img" src="<?php echo get_template_directory_uri(); ?>'/img/Nowadays_micro_dose.webp'">
+                </div>
+                <div class="sn-product__info">
+                    <h2 class="sn-product__title ">Nowadays high dose</h2>
+                    <div class="sn-product__reviews">
+                        <input type="radio" id="star16" name="rating3" value="5"/>
+                        <label for="star16">&#9733;</label>
+                        <input type="radio" id="star15" name="rating3" value="4"/>
+                        <label for="star15">&#9733;</label>
+                        <input type="radio" id="star14" name="rating3" value="3"/>
+                        <label for="star14">&#9733;</label>
+                        <input type="radio" id="star12" name="rating3" value="2"/>
+                        <label for="star12">&#9733;</label>
+                        <input type="radio" id="star11" name="rating3" value="1"/>
+                        <label for="star11">&#9733;</label>
+                    </div>
+                    <span class="sn-product__span">98 reviews</span>
+                    <p class="sn-product__description">10mg thc drink</p>
+                    <p class="sn-product__bottle">750 ml/166mg per bottle</p>
+                    <p class="sn-product__price">$79.99</p>
+                    <a href="#" class=" btn btn__beige--small">Add to card</a>
+                </div>
+            </div>
+            <div class="sn-product">
+                <div class="sn-product__image ">
+                    <img class="sn-product__infocards" src="<?php echo get_template_directory_uri(); ?>/img/PLP_infoCards_1.png'">
+                </div>
+        </div>
+        </div>
+    </div>
+</section>
+<div class="shop-all">
+    <a href="#" class=" btn btn__white--small">Shop all</a>
+</div>
+
+<!--section merch-->
+<section class="merch">
+    <div class="container">
+        <div class="merch__head">
+            <h2 class="merch__title">SHOP NOWADAYS</h2>
+        </div>
+        <div class="merch__products">
+            <div class="merch-product">
+                <div class="merch-product__image merch-product__one">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/Nowadays_micro_dose.webp'">
+                    <?php $background_product = get_field( 'background_product' ); ?>
+                </div>
+                <div class="merch-product__info">
+                    <h2 class="merch-product__title">Nowadays micro dose</h2>
+                    <div class="merch-product__reviews">
+                        <input type="radio" id="star5" name="rating" value="5"/>
+                        <label for="star5">&#9733;</label>
+                        <input type="radio" id="star4" name="rating" value="4"/>
+                        <label for="star4">&#9733;</label>
+                        <input type="radio" id="star3" name="rating" value="3"/>
+                        <label for="star3">&#9733;</label>
+                        <input type="radio" id="star2" name="rating" value="2"/>
+                        <label for="star2">&#9733;</label>
+                        <input type="radio" id="star1" name="rating" value="1"/>
+                        <label for="star1">&#9733;</label>
+                    </div>
+                    <span class="merch-product__span">67 reviews</span>
+                    <p class="merch-product__description">2mg thc drink</p>
+                    <p class="merch-product__bottle">750 ml/33mg per bottle</p>
+                    <p class="merch-product__price">$39.99</p>
+                    <a href="#" class=" btn btn__beige--small">Add to card</a>
+                </div>
+            </div>
+            <div class="merch-product">
+                <div class="merch-product__image sn-product__two">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/Nowadays_micro_dose.webp'">
+                </div>
+                <div class="merch-product__info">
+                    <h2 class="merch-product__title ">Nowadays low dose</h2>
+                    <div class="merch-product__reviews">
+                        <input type="radio" id="star10" name="rating1" value="5"/>
+                        <label for="star10">&#9733;</label>
+                        <input type="radio" id="star9" name="rating1" value="4"/>
+                        <label for="star9">&#9733;</label>
+                        <input type="radio" id="star8" name="rating1" value="3"/>
+                        <label for="star8">&#9733;</label>
+                        <input type="radio" id="star7" name="rating1" value="2"/>
+                        <label for="star7">&#9733;</label>
+                        <input type="radio" id="star6" name="rating1" value="1"/>
+                        <label for="star6">&#9733;</label>
+                    </div>
+                    <span class="merch-product__span">87 reviews</span>
+                    <p class="merch-product__description">6mg thc drink</p>
+                    <p class="merch-product__bottle">750 ml/100mg per bottle</p>
+                    <p class="merch-product__price">$59.99</p>
+                    <a href="#" class=" btn btn__beige--small">Add to card</a>
+                </div>
+            </div>
+            <div class="merch-product">
+                <div class="sn-product__image sn-product__three">
+                    <img class="merch-product__img" src="<?php echo get_template_directory_uri(); ?>'/img/Nowadays_micro_dose.webp'">
+                </div>
+                <div class="merch-product__info">
+                    <h2 class="merch-product__title ">Nowadays high dose</h2>
+                    <div class="merch-product__reviews">
+                        <input type="radio" id="star16" name="rating3" value="5"/>
+                        <label for="star16">&#9733;</label>
+                        <input type="radio" id="star15" name="rating3" value="4"/>
+                        <label for="star15">&#9733;</label>
+                        <input type="radio" id="star14" name="rating3" value="3"/>
+                        <label for="star14">&#9733;</label>
+                        <input type="radio" id="star12" name="rating3" value="2"/>
+                        <label for="star12">&#9733;</label>
+                        <input type="radio" id="star11" name="rating3" value="1"/>
+                        <label for="star11">&#9733;</label>
+                    </div>
+                    <span class="merch-product__span">98 reviews</span>
+                    <p class="merch-product__description">10mg thc drink</p>
+                    <p class="merch-product__bottle">750 ml/166mg per bottle</p>
+                    <p class="merch-product__price">$79.99</p>
+                    <a href="#" class=" btn btn__beige--small">Add to card</a>
+                </div>
+            </div>
+            <div class="merch-product">
+                <div class="merch-product__image ">
+                    <img class="merch-product__infocards" src="<?php echo get_template_directory_uri(); ?>/img/PLP_infoCards_1.png'">
+                </div>
+        </div>
+        </div>
+    </div>
+</section>
+<div class="shop-all">
+    <a href="#" class=" btn btn__white--small">Shop all</a>
+</div>
+
+
+
+<?php get_footer(); ?>
