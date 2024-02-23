876546d56b828c84cdd68882bfc35cac9b3d9376
Francisco Duran <frandugo@ymail.com>
2024-02-23 07:37:22 -0500

    add changes to plp

--- plp.php
+++ plp.php
@@ -1,3 +1,4 @@
+<?php /* Template Name: plp */ ?>
 <?php get_header(); ?>
 <div class="plp-hero" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/nowadays-hero-img.png')">
     <div class="container">
